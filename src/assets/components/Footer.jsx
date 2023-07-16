deb22b5761e50a712f9370be94e4e1d5dad1ab82
Marvelousmicheal <marvelousdonatus09@gmail.com>
2023-07-16 22:33:09 +0100

    republish the code and added the last section

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Buttons from "./Buttons";
 import { images } from "../data/imageDate";
-import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   const selectedImage1 = images[13];
   return (
     <>
-      <footer className="bg-black">
+      <footer className="bg-[#212121]">
         <div className="container mx-auto py-10 px-4 lg:px-0">
           <div className="flex flex-wrap justify-between items-center  mb-7">
             <div className=" bg-white px-2 py-2 h-fit rounded-lg lg:w-[30%] mb-4 lg:mb-0">
@@ -23,24 +23,18 @@ function Footer() {
             </div>
 
             <div
-              className="flex space-x-5
+              className="flex space-x-5 w-full lg:w-auto
         "
             >
-              <img
-                src="/images/facebook logo.png"
-                alt="facebook-link"
-                className="w-[50px] h-[50px]"
-              />
-              <img
-                src="/images/instag.png"
-                alt="instag-link"
-                className="w-[50px] h-[50px]"
-              />
-              <img
-                src="/images/twitter.png"
-                alt="twitter-link"
-                className="w-[50px] h-[50px]"
-              />
+              <span className="bg-gray-700 lg:w-10 w-8 lg:h-10 h-8 flex justify-center items-center rounded-full">
+                <FaFacebook className="text-white lg:text-2xl text-xl" />
+              </span>
+              <span className="bg-gray-700 lg:w-10 w-8 lg:h-10 h-8  flex justify-center items-center rounded-full">
+                <FaTwitter className="text-white  lg:text-2xl text-xl" />
+              </span>
+              <span className="bg-gray-700 lg:w-10 w-8 lg:h-10 h-8  flex justify-center items-center rounded-full">
+                <FaInstagram className="text-white lg:text-2xl text-xl" />
+              </span>
             </div>
           </div>
           <div>
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
   const selectedImage13 = images[4];
   const selectedImage14 = images[5];
   const totalStars = 5;
-  const rating = 4; // Rating of the stars (can be dynamic or from data)
+  const rating = 4; 
   return (
     <>
       <section>
@@ -31,7 +31,7 @@ function Home() {
           <div className="h-[87%]">
             <main className="bg-[#0d9733] lg:h-[1007px] rounded-3xl text-white  md:px-0 px-4 h-full  ">
               <div className="pt-[4rem]  space-y-5">
-                <h1 className=" text-4xl leading-10 lg:text-[55px] font-bold lg:leading-[4rem] md:text-center text-j">
+                <h1 className=" text-4xl leading-10 lg:text-[55px] font-bold lg:leading-[4rem] md:text-center">
                   Never Queue Again!
                   <br className="hidden md:block" /> Refill Your Cooking Gas,
                   Fuel or
